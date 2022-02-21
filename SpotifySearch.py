e179e223476003d4de44a9d6e4621c73e52592f1
LeFamil99 <maxgaudreau9@gmail.com>
2022-02-21 16:02:48 -0500

    Added spotify query

---
+++ SpotifySearch.py
@@ -0,0 +1,40 @@
+import requests
+import base64
+import json
+from secrets import *
+
+# Step 1 - Authorization 
+url = "https://accounts.spotify.com/api/token"
+headers = {}
+data = {}
+
+# Encode as Base64
+message = f"{clientId}:{clientSecret}"
+messageBytes = message.encode('ascii')
+base64Bytes = base64.b64encode(messageBytes)
+base64Message = base64Bytes.decode('ascii')
+
+
+headers['Authorization'] = f"Basic {base64Message}"
+data['grant_type'] = "client_credentials"
+
+r = requests.post(url, headers=headers, data=data)
+
+token = r.json()['access_token']
+
+length = 10
+
+searchQuery = "runaway"
+searchUrl = f"https://api.spotify.com/v1/search?q=track:{searchQuery}&type=track&limit={length}"
+headers = {
+    "Authorization": "Bearer " + token
+}
+
+res = requests.get(url=searchUrl, headers=headers)
+
+resd = json.dumps(res.json(), indent=2)
+
+print(resd)
+for i in range(length):
+    print(res.json()["tracks"]["items"][i]["external_urls"]["spotify"])
+
