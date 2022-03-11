1d3845fadde38747ca035a1183118b61b3b7fc03
liyan19 <eliassenoune@hotmail.com>
2022-03-11 15:46:20 -0500

    Update WikipediaSearch.py

--- WikipediaSearch.py
+++ WikipediaSearch.py
@@ -1,7 +1,19 @@
 import urllib.request
+import wikipediaapi
+ 
+wiki_wiki = wikipediaapi.Wikipedia('fr')
+page_py=wiki_wiki.page('Kanye_West')
 
-ArtistName = "Kanye West"
-ArtistName = ArtistName.replace(' ', '_')
-html = urllib.request.urlopen("https://en.wikipedia.org/wiki/" + ArtistName)
+print("Page - Title: %s" % page_py.title)
+ 
 
-print(html.read().decode())
+print("Page - Summary: %s" % page_py.summary[0:1000].split("\n")[1])
+   
+
+
+
+#ArtistName = "Kanye West"
+#ArtistName = ArtistName.replace(' ', '_')
+#html = urllib.request.urlopen("https://en.wikipedia.org/wiki/" + ArtistName)
+
+#print(html.read().decode())
