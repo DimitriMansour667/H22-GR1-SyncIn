d174575b5304642118a31438f8d3b884db4d9e2e
LeFamil99 <maxgaudreau9@gmail.com>
2022-05-12 11:56:22 -0400

    Ajout de commentaires

--- SyncIn/search/urls.py
+++ SyncIn/search/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+# Permet de lier le backend et le frontend à l'aide d'un path URL
 urlpatterns = [
     path("", views.search, name="search"),
-    path("result/", views.results, name="result"),
+    path("result/<slug:offset>", views.results, name="result"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
